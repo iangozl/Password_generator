19c406e65d5839376efb6a8ce071c622b152b1ad
iangozl <ig_computing@protonmail.com>
2021-02-21 00:08:22 -0500

    new commit

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,12 +1,12 @@
-"""
+""" 
     Write a programme, which generates a random password for the user. Ask the user how long they 
     want their password to be, and how many letters and numbers they want in their password. 
     Have a mix of upper and lowercase letters, as well as numbers and symbols. 
     The password should be a minimum of 6 characters long.
 """ 
-import string
-import random
-import array as arr
+import string 
+import random 
+import array as arr 
 
 # All the variables
 
@@ -17,17 +17,17 @@ pwd_length = 0
 # Verification
 
 while pwd_length < 6:
-
+    
     try:
         pwd_length = int(input("How many characters will your password have?:\n"))
-        if pwd_length >= 6:
-            
-            pwd_list = random.choices(all_char, k = pwd_length)
+        if pwd_length >= 6: 
+
+            pwd_list = random.choices(all_char, k = pwd_length) 
 
             print('Your password is:',''.join(x for x in pwd_list))
         else:
-            print('It should be greater than 6 digits!')
+            print('It should be greater than 6 digits!') 
 
     except:
-        print("Please enter an integer number")
-
+        
+        print("Please enter an integer number") 
